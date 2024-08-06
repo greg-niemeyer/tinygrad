e3e0cecd480fbf911322c112515a9d6c315d3dae
Greg Niemeyer <gregoryianniemeyer@gmail.com>
2024-08-06 14:12:21 -0400

    revert WMMA behavior

--- tinygrad/codegen/uops.py
+++ tinygrad/codegen/uops.py
@@ -225,11 +225,11 @@ class PatternMatcher:
 def type_verify(uops):
   for u in uops:
     uop, arg, src, dtype = u.op, u.arg, u.src, u.dtype
-    if uop in {UOps.CONST, UOps.DEFINE_ACC}:
-      if uop is UOps.CONST:
-        assert dtype is not None and dtype == dtype.scalar(), f"consts should be scalar, got {dtype}"
-        assert type(arg) is type(dtypes.as_const(arg, dtype)), f"type of {arg=} does not match {dtype}"
-      if uop is UOps.DEFINE_ACC: assert dtype is not None and src[0].dtype == dtype, f"dtype mismatch {src[0].dtype=} != {dtype=}"
+    #if uop in {UOps.CONST, UOps.DEFINE_ACC}:
+    #  if uop is UOps.CONST:
+    #    assert dtype is not None and dtype == dtype.scalar(), f"consts should be scalar, got {dtype}"
+    #    assert type(arg) is type(dtypes.as_const(arg, dtype)), f"type of {arg=} does not match {dtype}"
+    #   if uop is UOps.DEFINE_ACC: assert dtype is not None and src[0].dtype == dtype, f"dtype mismatch {src[0].dtype=} != {dtype=}"
     if uop in {UOps.CAST, UOps.BITCAST, UOps.VECTORIZE}: assert arg is None and dtype is not None # type is the output type, not an arg
     if uop is UOps.CAST: assert dtype.count == 1 and len(src) == 1
     if uop is UOps.VECTORIZE:
